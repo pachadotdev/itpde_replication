e07015c7c21136407c4065a671f4b93cd81bf462
Pacha <mavargas11@uc.cl>
2022-10-23 20:46:55 -0400

    progress converting FAOSTAT codes

--- 02-agricultural-trade-data-faostat.R
+++ 02-agricultural-trade-data-faostat.R
@@ -7,22 +7,29 @@ library(purrr)
 
 # 1: Download trade data ----
 
-url <- "https://fenixservices.fao.org/faostat/static/bulkdownloads/Trade_DetailedTradeMatrix_E_All_Data.zip"
-zip <- "inp/zip/faostat_trade_matrix.zip"
+url_trade_data <- "https://fenixservices.fao.org/faostat/static/bulkdownloads/Trade_DetailedTradeMatrix_E_All_Data.zip"
+zip_trade_data <- "inp/zip/faostat_trade_matrix.zip"
 
 try(dir.create("inp/zip", recursive = T))
 
-if (!file.exists(zip)) {
-  download.file(url, zip)
+if (!file.exists(zip_trade_data)) {
+  download.file(url_trade_data, zip_trade_data)
 }
 
 if (length(list.files("inp/csv/faostat_trade_matrix")) == 0) {
-  archive_extract(zip, dir = "inp/csv/faostat_trade_matrix")
+  archive_extract(zip_trade_data, dir = "inp/csv/faostat_trade_matrix")
 }
 
-# 2: Download FAOSTAT Commodity List ----
+# 2: Download FCL (FAOSTAT) to ITPD-E codes ----
 
-# 3: Download correspondence between FAO country codes and ISO-3 country codes ----
+url_fcl_id <- "https://www.usitc.gov/data/gravity/itpde_concordances/itpd_e_r02_ag_fcl.csv"
+csv_fcl_id <- "inp/csv/fcl_to_itpde.csv"
+
+if (!file.exists(csv_fcl_id)) {
+  download.file(url_fcl_id, csv_fcl_id)
+}
+
+# 2: Download correspondence between FAO country codes and ISO-3 country codes ----
 
 # the file was downloaded from the browser and points to
 # blob:https://www.fao.org/57203009-07f0-4867-9eaf-81a56b990566
@@ -30,12 +37,33 @@ if (length(list.files("inp/csv/faostat_trade_matrix")) == 0) {
 # (inp/img/faostats_country_correspondence.png)
 # the file is inp/csv/faostat_country_correspondence.csv
 
-# 4: Tidy data ----
+# 3: Download correspondence between FAO product codes and HS product codes ----
+
+# the file was downloaded from the browser and points to
+# blob:https://www.fao.org/57203009-07f0-4867-9eaf-81a56b990566
+# see image for https://www.fao.org/faostat/en/#data/QV
+# (inp/img/faostats_country_correspondence.png)
+# the file is inp/csv/faostat_country_correspondence.csv
+
+# 4: Download HS 92 to ISIC 3 correspondence table ----
+
+url_fcl_hs <- "http://wits.worldbank.org/data/public/concordance/Concordance_H0_to_I3.zip"
+zip_fcl_hs <- "inp/zip/hs92_to_isic3.zip"
+
+if (!file.exists(zip_fcl_hs)) {
+  download.file(url_fcl_hs, zip_fcl_hs)
+}
+
+if (length(list.files("inp/csv/hs92_to_isic3")) == 0) {
+  archive_extract(zip_fcl_hs, dir = "inp/csv/hs92_to_isic3")
+}
+
+# 5: Tidy data ----
+
+## 5.1 tidy codes ----
 
 fao_data <- read_csv("inp/csv/faostat_trade_matrix/Trade_DetailedTradeMatrix_E_All_Data_NOFLAG.csv") %>%
   clean_names()
-
-## 4.1 tidy codes ----
 
 fao_element_code <- fao_data %>%
   select(element_code, element) %>%
@@ -69,7 +97,7 @@ fao_data <- fao_data %>%
   left_join(fao_unit_code) %>%
   select(reporter_country_code:element_code, unit_code, y1986:y2020)
 
-## 4.2 convert wide to long ----
+## 5.2 convert wide to long ----
 
 fao_data <- fao_data %>%
   group_by(reporter_country_code) %>%
@@ -129,7 +157,7 @@ fao_data <- fao_data %>%
     import_quantity_no_unit = import_quantity_no
   )
 
-## 4.3 convert country codes ----
+## 5.3 convert country codes ----
 
 fao_country_correspondence <- read_csv("inp/csv/faostat_country_correspondence.csv") %>%
   clean_names() %>%
@@ -148,7 +176,40 @@ fao_data <- fao_data %>%
   ungroup() %>%
   select(-c(reporter_country_code, partner_country_code))
 
-# 5: save ----
+# 6: Convert FCL to ITPD-E, filter and aggregate ----
+
+fcl_to_itpde <- read_csv("inp/csv/fcl_to_itpde.csv") %>%
+  clean_names() %>%
+  select(industry_id = itpd_id, item_code = fcl_item_code) %>%
+  mutate_if(is.double, as.integer)
+
+fao_data <- fao_data %>%
+  inner_join(fcl_to_itpde, by = "item_code") %>%
+  group_by(year, reporter_iso3, partner_iso3, industry_id) %>%
+  summarise(import_value_usd = sum(import_value_usd, na.rm = T))
+
+fao_data %>%
+  filter(reporter_iso3 == "CHL", partner_iso3 == "BRA", year == 2010L,
+         industry_id %in% unique(fcl_to_itpde$industry_id)) %>%
+  mutate(import_value_usd = import_value_usd / 1000000) %>%
+  arrange(industry_id)
+
+library(usitcgravity)
+
+con <- usitcgravity_connect()
+
+industries <- unique(fcl_to_itpde$industry_id)
+
+tbl(con, "trade") %>%
+  filter(importer_iso3 == "CHL", exporter_iso3 == "BRA", year == 2010L,
+         industry_id %in% industries) %>%
+  arrange(industry_id) %>%
+  select(exporter_iso3, importer_iso3, industry_id, trade) %>%
+  collect()
+
+# I need to check these values
+
+# X: save ----
 
 try(dir.create("out/rds", recursive = T))
 
