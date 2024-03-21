c64f6f50aa349d30447c4c221870c4e11b9fa664
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-03-21 19:30:35 -0400

    improve itpde copy

--- 02-copy-itpde-tables.R
+++ 02-copy-itpde-tables.R
@@ -18,26 +18,28 @@ con <- dbConnect(
 )
 
 if (!"usitc_trade" %in% dbListTables(con)) {
-  # what is wrong with this ... I need to download from the browser
+  # I needed to download from the browser or it doesn't work
+  # https://www.usitc.gov/data/gravity/itpd_e/itpd_e_r02.zip
   if (!file.exists(paste0(finp, "/ITPD_E_R02.rds"))) {
     if (!length(list.files(finp, pattern = "ITPD_E_R02\\.csv")) == 1) {
       archive_extract(zip, dir = finp)
     }
 
     # this table is to compare replication values later
-    # this file is super heavy and takes a lot of time to read, it crashed pandas :(
+    # this file is super heavy and takes a lot of time to read
+    # it crashed pandas :(
     trade <- read_csv(
       paste0(finp, "/ITPD_E_R02.csv"),
       col_types = cols(
         year = col_integer(),
         industry_id = col_integer(),
-        trade = col_double(),
-        exporter_dynamic_code = col_skip(),
-        exporter_name = col_skip(),
-        importer_dynamic_code = col_skip(),
-        importer_name = col_skip()
+        trade = col_double()
       )
     )
+
+    # reduce weight a bit using factors
+    trade <- trade %>%
+      mutate_if(is.character, as.factor)
 
     gc()
 
@@ -52,67 +54,75 @@ if (!"usitc_trade" %in% dbListTables(con)) {
 
   # this table is to avoid writing special country codes that we won't need
   country_names <- trade %>%
-    select(country_iso3 = exporter_iso3) %>%
+    select(
+      country_iso3 = exporter_iso3,
+      country_dynamic_code = exporter_dynamic_code,
+      country_name = exporter_name
+    ) %>%
     distinct() %>%
+    mutate_if(is.factor, as.character) %>%
     bind_rows(
       trade %>%
-        select(country_iso3 = importer_iso3) %>%
-        distinct()
+        select(
+          country_iso3 = importer_iso3,
+          country_dynamic_code = importer_dynamic_code,
+          country_name = importer_name
+        ) %>%
+        distinct() %>%
+        mutate_if(is.factor, as.character)
     ) %>%
     distinct() %>%
     arrange(country_iso3)
 
+  dbWriteTable(con, "usitc_country_codes", country_names, overwrite = T)
+
+  trade <- trade %>%
+    select(-exporter_name, -importer_name)
+
   # just for references
   industry_names <- trade %>%
     select(industry_id, industry_descr) %>%
-    distinct()
+    distinct() %>%
+    mutate_if(is.factor, as.character)
 
-  dbSendQuery(
-    con,
-    "CREATE TABLE usitc_country_codes (
-	    country_iso3 text NULL
-    )"
-  )
+  dbWriteTable(con, "usitc_industry_names", industry_names, overwrite = T)
 
-  dbSendQuery(
-    con,
-    "CREATE TABLE usitc_industry_names (
-      industry_id int4 NULL,
-      industry_descr text NULL
-    )"
-  )
-
-  dbWriteTable(con, "usitc_country_codes", country_names, append = T)
-
-  dbWriteTable(con, "usitc_industry_names", industry_names, append = T)
+  trade <- trade %>%
+    select(-industry_descr)
 
   sector_names <- trade %>%
     select(broad_sector) %>%
     distinct() %>%
+    mutate_if(is.factor, as.character) %>%
     arrange(broad_sector) %>%
     mutate(broad_sector_id = row_number()) %>%
     select(broad_sector_id, broad_sector)
 
-  sector_names_2 <- trade %>%
-    select(broad_sector) %>%
-    inner_join(sector_names)
-
-  trade <- trade %>%
-    select(-broad_sector, -industry_descr)
-
-  trade <- trade %>%
-    bind_cols(sector_names_2) %>%
-    select(
-      year, exporter_iso3, importer_iso3, broad_sector_id,
-      industry_id, trade, flag_mirror, flag_zero
-    )
+  dbWriteTable(con, "usitc_sector_names", sector_names, overwrite = T)
 
   trade <- trade %>%
     mutate(
-      year = as.integer(year),
-      broad_sector_id = as.integer(broad_sector_id),
-      industry_id = as.integer(industry_id)
+      broad_sector_id = case_when(
+        broad_sector == "Agriculture" ~ 1L,
+        broad_sector == "Manufacturing" ~ 2L,
+        broad_sector == "Mining and Energy" ~ 3L,
+        broad_sector == "Services" ~ 4L
+      )
+    ) %>%
+    select(-broad_sector)
+
+  trade <- trade %>%
+    select(
+      year,
+      exporter_iso3, importer_iso3,
+      exporter_dynamic_code, importer_dynamic_code,
+      broad_sector_id, industry_id,
+      flag_mirror, flag_zero,
+      trade
     )
+
+  trade <- trade %>%
+    mutate(flag_mirror = as.integer(flag_mirror))
 
   trade <- trade %>%
     group_by(year) %>%
@@ -120,20 +130,12 @@ if (!"usitc_trade" %in% dbListTables(con)) {
 
   gc()
 
-  dbSendQuery(
-    con,
-    "CREATE TABLE usitc_trade (
-    	year int4 NULL,
-    	exporter_iso3 text NULL,
-    	importer_iso3 text NULL,
-    	broad_sector_id int4 NULL,
-    	industry_id int4 NULL,
-    	trade float8 NULL,
-    	flag_mirror int4 NULL,
-    	flag_zero text NULL
-    )"
-  )
+  # drop usitc_trade if exists
+  if ("usitc_trade" %in% dbListTables(con)) {
+    dbRemoveTable(con, "usitc_trade")
+  }
 
+  # copy year by year
   map(
     1986:2020,
     function(y) {
@@ -141,13 +143,14 @@ if (!"usitc_trade" %in% dbListTables(con)) {
 
       d <- trade %>%
         filter(year == y) %>%
-        unnest(data)
+        unnest(data) %>%
+        mutate_if(is.factor, as.character)
 
       dbWriteTable(con, "usitc_trade", d, overwrite = F, append = T)
     }
   )
 
-  rm(trade, sector_names, sector_names_2)
+  rm(trade, country_names, industry_names, sector_names)
   gc()
 }
 
